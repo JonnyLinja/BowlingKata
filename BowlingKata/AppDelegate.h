025f77d38b0ea1b6d3a6744093cfb982adbc215e
JonnyLinja <xxmy_emailxx@yahoo.com>
2015-05-23 20:46:31 -0400

    Initial commit

---
+++ BowlingKata/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BowlingKata
+//
+//  Created by N A on 5/23/15.
+//  Copyright (c) 2015 N A. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BowlingKata/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BowlingKata
+//
+//  Created by N A on 5/23/15.
+//  Copyright (c) 2015 N A. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
